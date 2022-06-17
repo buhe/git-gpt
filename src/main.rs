3c04a59ef6bc5536b3a0a25cbe7d7a05bf496ab4
buhe <bugu1986@gmail.com>
2022-06-17 14:20:34 +0800

    git-u:update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use git2::{Repository, Index, ObjectType, Signature, Error, Direction};
 
 fn main() {
@@ -83,6 +85,30 @@ fn push(repo: &Repository) -> Result<(), git2::Error> {
         Ok(r) => r,
         Err(_) => repo.remote("origin", "unknow")?,
     };
-    remote.connect(Direction::Push)?;
+    // remote.connect(Direction::Push)?;
+    let mut callbacks = git2::RemoteCallbacks::new();
+    callbacks.credentials(git_credentials_callback);
+    remote.connect_auth(Direction::Push, Some(callbacks), None)?;
     remote.push(&["refs/heads/master:refs/heads/master"], None)
+}
+
+pub fn git_credentials_callback(
+    _user: &str,
+    _user_from_url: Option<&str>,
+    _cred: git2::CredentialType,
+) -> Result<git2::Cred, git2::Error> {
+    let user = _user_from_url.unwrap_or("git");
+
+    if _cred.contains(git2::CredentialType::USERNAME) {
+        return git2::Cred::username(user);
+    }
+
+    match env::var("GPM_SSH_KEY") {
+        Ok(k) => {
+
+            println!("authenticate with user {} and private key located in {}", user, k);
+            git2::Cred::ssh_key(user, None, std::path::Path::new(&k), None)
+        },
+        _ => Err(git2::Error::from_str("unable to get private key from GPM_SSH_KEY")),
+    }
 }
