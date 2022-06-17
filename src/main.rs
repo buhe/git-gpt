79e61a1f6bb5921a8f5b90ca39bef8a4f9aab214
buhe <bugu1986@gmail.com>
2022-06-17 13:51:03 +0800

    git-u:update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{path::Path};
 
-use git2::{Repository, Index, ObjectType, Signature};
+use git2::{Repository, Index, ObjectType, Signature, Error};
 
 fn main() {
     println!("Hello, git!");
@@ -15,6 +15,7 @@ fn run() -> Result<(), git2::Error> {
     let repo = open()?;
     let mut index = add_all(&repo)?;
     commit(&repo, &mut index)?;
+    pull(&repo)?;
     // let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
     // let commit = obj.into_commit().map_err(|_| git2::Error::from_str("Couldn't find commit"))?;
     // println!("commit {}\nAuthor: {}\n    {}",
@@ -39,7 +40,7 @@ fn add_all(repo: &Repository) -> Result<Index, git2::Error> {
     Ok(index)
 }
 
-fn commit(repo: &Repository, index: &mut Index) -> Result<(), git2::Error>  {
+fn commit(repo: &Repository, index: &mut Index) -> Result<(), git2::Error> {
      let oid = index.write_tree()?;
     let signature = Signature::now("buhe", "bugu1986@gmail.com")?;
     let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
@@ -55,8 +56,24 @@ fn commit(repo: &Repository, index: &mut Index) -> Result<(), git2::Error>  {
     Ok(())
 }
 
-fn pull() {
+fn pull(repo: &Repository) -> Result<(), git2::Error> {
+    repo.find_remote("origin")?
+        .fetch(&["master"], None, None)?;
 
+    let fetch_head = repo.find_reference("FETCH_HEAD")?;
+    let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
+    let analysis = repo.merge_analysis(&[&fetch_commit])?;
+    if analysis.0.is_up_to_date() {
+        Ok(())
+    } else if analysis.0.is_fast_forward() {
+        let refname = format!("refs/heads/{}", "master");
+        let mut reference = repo.find_reference(&refname)?;
+        reference.set_target(fetch_commit.id(), "Fast-Forward")?;
+        repo.set_head(&refname)?;
+        repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))
+    } else {
+        Err(Error::from_str("Fast-forward only!"))
+    }
 }
 
 fn push() {
