e05ad7b5b3875f3712d83e711bdaa24923a4b230
buhe <bugu1986@126.com>
2023-03-04 11:11:04 +0800

    git-u:update

--- src/sdk.rs
+++ src/sdk.rs
@@ -6,7 +6,7 @@ pub struct GPT {
 
 impl GPT {
     pub fn setup(&self){
-         match env::var("ATPH") {
+         match env::var("PATH") {
             Ok(val) => println!("PATH is {}", val),
             Err(e) => println!("couldn't read PATH: {}", e),
         }
