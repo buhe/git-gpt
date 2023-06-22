d789f059d70ea5720be7876a5f4d823b4ae37d65
buhe <bugu1986@126.com>
2023-06-22 13:20:49 +0800

    "Add LICENSE file to the list of files excluded from Git diff in main.rs."

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ fn skip_diff(mut command: Command) -> Command {
         .arg("--cached")
         .arg("--")
         .arg(".");
-    for arg in vec!["':!.vscode'", "':!*.lock'"].into_iter() {
+    for arg in vec!["':!.vscode'", "':!*.lock'", "LICENSE"].into_iter() {
         command.arg(arg);
     }
     command
